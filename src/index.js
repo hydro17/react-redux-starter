af0949dcb2358341ca254fc0ec0a7c07636ac510
Andrzej Pepłowski <pirx555@gmail.com>
2018-03-02 22:51:57 +0100

    Refactor the App component. Add YouTubeSearch function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-const React = require('react');
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
-
 const API_KEY = 'AIzaSyDqejb164mXGK-igUstESqPUxVsMKHBcmU';
 
-const App = () => <div><SearchBar /></div>;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container')); 
