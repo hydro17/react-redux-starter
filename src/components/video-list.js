46349e78f5bf38a324e1118d5082bba030493744
Andrzej Pepłowski <pirx555@gmail.com>
2018-03-04 00:00:22 +0100

    Updated component VideoListItem

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -2,7 +2,8 @@ import React from 'react';
 import VideoListItem from './video-list-item';
 
 const VideoList = (props) => {
-  const videoItems = props.videos.map(video => <VideoListItem video={video} />)
+  const videoItems = props.videos.map(video =>
+    <VideoListItem key={video.etag} video={video} />)
 
   return (
     <ul className='list-group'>
