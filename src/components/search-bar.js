8871a54213f660365d869e1d55668be2e17aa7fc
Andrzej Pepłowski <pirx555@gmail.com>
2018-03-07 20:52:12 +0100

    Update the component SearchBar

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -9,16 +9,21 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
 
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
+
   render() {
     return (
       <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={e => this.setState({ term: e.target.value })}
-          onKeyDown={e => {
-            // if (e.which === 13) this.props.onVideoSearch(this.state.term);
-            if (e.key === 'Enter') this.props.onSearchTermChange(this.state.term);
-          }}
+          onChange={(event) => this.onInputChange(event.target.value)}
+        // onKeyDown={e => {
+        //   // if (e.which === 13) this.props.onVideoSearch(this.state.term);
+        //   if (e.key === 'Enter') this.props.onSearchTermChange(this.state.term);
+        // }}
         />
       </div>
     );
